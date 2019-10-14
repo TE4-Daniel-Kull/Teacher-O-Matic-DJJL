30216c4c6d52ed293ecf22fe29ceba47cf0505e5
Valen98 <leo.wahlandtcs@gmail.com>
2019-10-14 15:02:11 +0200

    fixed the API request limit

--- js/api.js
+++ js/api.js
@@ -1,7 +1,8 @@
 export class API {
 
+
   static async search(value) {
-    const response = await fetch(`https://api.github.com/users/${value}/repos`)
+    const response = await fetch(`https://api.github.com/users/${value}/repos?client_id=d8e0bd6b8e516a3a6a83`)
     const data = await response.json()
     return data
   }
